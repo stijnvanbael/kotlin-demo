bf57bdaa4d5b6c54ed2561bf3fb18912c85adb88
Stijn Van Bael <stijn.van.bael@conundra.eu>
2021-04-26 14:38:02 +0200

    Added more Kotlin features

--- src/main/java/demo/kotlin/13_TypeAlias.kt
+++ src/main/java/demo/kotlin/13_TypeAlias.kt
@@ -1,2 +1,13 @@
 package demo.kotlin
 
+typealias Predicate<V> = (V) -> Boolean
+typealias Strings = List<String>
+
+fun main(args: Array<String>) {
+    val transformed = transform(listOf("one", "two", "three")) { string -> string.contains('o') }
+    println(transformed)
+}
+
+fun transform(strings: Strings, predicate: Predicate<String>) =
+    strings.filter(predicate).map { string -> string.toUpperCase() }
+
